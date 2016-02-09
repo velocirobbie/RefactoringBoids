72112c2afdb9d3fa6afb5de5f4bb2a2d194457f7
Robert Sinclair <rcsinclair3@gmail.com>
2016-02-09 20:17:18 +0000

    added unittest for match_speeds, tidied test functions

--- simulation/test_class_boids.py
+++ simulation/test_class_boids.py
@@ -4,28 +4,31 @@ from nose.tools import assert_equal,assert_almost_equal
 import os
 import yaml
 
-def test_fly_towards_middle():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'tests/fixtures/regression_fixtures.yml')))
-    pos_start = [regression_data['before'][0],regression_data['before'][1]]
-    vel_start = [regression_data['before'][2],regression_data['before'][3]]
-    
+def init_boids():
+    pathtofile = 'tests/fixtures/regression_fixtures.yml'
+    data=yaml.load(open(os.path.join(os.path.dirname(__file__),pathtofile)))
+    pos_start = [data['before'][0],data['before'][1]]
+    vel_start = [data['before'][2],data['before'][3]]
     test_boids = Boids(pos_start,vel_start)
-    test_boids.fly_towards_middle(0.01)
+    return test_boids
+
+def check_func(test_boids,pathtofile):
     test_boids.increment_positions()
-    answer=yaml.load(open(os.path.join(os.path.dirname(__file__),'tests/fixtures/fly_towards_middle.yml')))
+    answer=yaml.load(open(os.path.join(os.path.dirname(__file__),pathtofile)))
     assert_almost_equal(test_boids.pos,answer['positions'],delta=0.01)
     assert_almost_equal(test_boids.vel,answer['velocities'],delta=0.01)
 
+def test_fly_towards_middle():
+    test_boids = init_test_boids() 
+    test_boids.fly_towards_middle(0.01)
+    check_func(test_boids,'tests/fixtures/fly_towards_middle.yml')
 
 def test_avoid_nearby_boids():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'tests/fixtures/regression_fixtures.yml')))
-    pos_start = [regression_data['before'][0],regression_data['before'][1]]
-    vel_start = [regression_data['before'][2],regression_data['before'][3]]
-    
-    test_boids = Boids(pos_start,vel_start)
+    test_boids = init_test_boids()
     test_boids.avoid_nearby_boids(100)
-    test_boids.increment_positions()
-    answer=yaml.load(open(os.path.join(os.path.dirname(__file__),'tests/fixtures/avoid_nearby_birds.yml')))
-    assert_almost_equal(test_boids.pos,answer['positions'],delta=0.01)
-    assert_almost_equal(test_boids.vel,answer['velocities'],delta=0.01)
+    check_func(test_boids,'tests/fixtures/avoid_nearby_birds.yml')
 
+def test_match_speeds():
+    test_boids = init_test_boids()
+    test_boids.match_speeds(0.125,1000)
+    check_func(test_boids,'tests/fixtures/match_speeds.yml')
