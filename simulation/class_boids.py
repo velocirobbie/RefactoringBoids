7c5cb35e742563d00c41af28d7fcc33b3ec1525c
Robert Sinclair <rcsinclair3@gmail.com>
2016-02-10 11:37:04 +0000

    updated fly towards middle to vectors, tests now itterate through array as truth of matrix1 = matrix2 is ambiguous

--- simulation/class_boids.py
+++ simulation/class_boids.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Boids(object):
     def __init__(self,positions,velocities):
         self.pos = positions
@@ -5,11 +7,10 @@ class Boids(object):
         self.Nboids = len(positions[1])
 
     def fly_towards_middle(self,coeff):
-        for i in range(self.Nboids):
-            for j in range(self.Nboids):
-                self.vel[0][i]=self.vel[0][i]+(self.pos[0][j]-self.pos[0][i])*coeff/self.Nboids
-                self.vel[1][i]=self.vel[1][i]+(self.pos[1][j]-self.pos[1][i])*coeff/self.Nboids
- 
+        middle = np.mean(self.pos,1)
+        direction_to_middle = self.pos - middle[:,np.newaxis]
+        self.vel -= direction_to_middle*coeff
+
     def avoid_nearby_boids(self,cutoff):
         for i in range(self.Nboids):
             for j in range(self.Nboids):
--- simulation/test_class_boids.py
+++ simulation/test_class_boids.py
@@ -15,20 +15,22 @@ def init_boids():
 def check_func(test_boids,pathtofile):
     test_boids.increment_positions()
     answer=yaml.load(open(os.path.join(os.path.dirname(__file__),pathtofile)))
-    assert_almost_equal(test_boids.pos,answer['positions'],delta=0.01)
-    assert_almost_equal(test_boids.vel,answer['velocities'],delta=0.01)
+    for j in range(len(test_boids.pos[1])):
+        for i in range(2):
+            assert_almost_equal(test_boids.pos[i][j],answer['positions'][i][j],delta=0.01)
+            assert_almost_equal(test_boids.vel[i][j],answer['velocities'][i][j],delta=0.01)
 
 def test_fly_towards_middle():
-    test_boids = init_test_boids() 
+    test_boids = init_boids() 
     test_boids.fly_towards_middle(0.01)
     check_func(test_boids,'tests/fixtures/fly_towards_middle.yml')
 
 def test_avoid_nearby_boids():
-    test_boids = init_test_boids()
+    test_boids = init_boids()
     test_boids.avoid_nearby_boids(100)
     check_func(test_boids,'tests/fixtures/avoid_nearby_birds.yml')
 
 def test_match_speeds():
-    test_boids = init_test_boids()
+    test_boids = init_boids()
     test_boids.match_speeds(0.125,1000)
     check_func(test_boids,'tests/fixtures/match_speeds.yml')
