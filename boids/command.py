c4ceb0f7774947a9f6a103ac657bc2740f7f2568
Robert Sinclair <rcsinclair3@gmail.com>
2016-02-27 16:06:51 +0000

    added argparse

--- boids/command.py
+++ boids/command.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
@@ -6,12 +7,23 @@ import os
 from class_boids import Boids
 from class_build import Builder
 
-
 def simulate():
-    config = yaml.load(open(os.path.join(
-        os.path.dirname(__file__),'config.yml')))
+    parser = ArgumentParser(
+            description='''Simulate the flocking behaviour of 
+            birds. Specify a configuration file or a default 
+            one will be used''')
+    parser.add_argument("--input", "-i",
+            help='Configuration file, in yaml format')
+    arguments = parser.parse_args()
+    
+    if arguments.input:
+        config = yaml.load(open(arguments.input))
+        initialise_simulation = Builder(arguments.input)
+    else:
+        config = yaml.load(open(os.path.join(
+            os.path.dirname(__file__),'config.yml')))
+        initialise_simulation = Builder('config.yml')
 
-    initialise_simulation = Builder('config.yml')
     positions = initialise_simulation.generate(
             'starting_positions')
     velocities = initialise_simulation.generate(
@@ -30,6 +42,7 @@ def simulate():
     anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
     plt.show()
+
 #if __name__ == "__main__":
 #    simulate()
 #    plt.show()
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 setup(
         name = 'boids',
-        version = '1.0.1',
+        version = '1.0.2',
         packages = find_packages(exclude=['*test']),
 #        data_files = [('boids',['boids/config.yml'])],
         package_data={"boids": ["config.yml"]},
         scripts = ['scripts/boids'],
-        install_requires = ['numpy','matplotlib']
+        install_requires = ['pyyaml','numpy','matplotlib']
         )
 
